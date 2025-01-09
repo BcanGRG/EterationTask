2af9acc9f608dee896ea00210095c57a303c320e
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-09 15:12:33 +0300

    added viewmodel and datas

--- app/src/main/java/com/bcan/eterationtask/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/bcan/eterationtask/presentation/home/HomeScreen.kt
@@ -8,20 +8,29 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bcan.eterationtask.presentation.home.components.HomeTopAppBar
 import com.bcan.eterationtask.presentation.home.components.ProductCard
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    viewModel: HomeViewModel = hiltViewModel()
+) {
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
     Scaffold(
         topBar = { HomeTopAppBar() },
     ) { innerPadding ->
@@ -39,22 +48,23 @@ fun HomeScreen() {
                     fontSize = 18.sp
                 )
                 Spacer(modifier = Modifier.height(24.dp))
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(2),
-                    horizontalArrangement = Arrangement.spacedBy(21.dp),
-                    verticalArrangement = Arrangement.spacedBy(14.dp),
-                ) {
-                    items(10) {
-                        ProductCard(
-                            imageUrl = "",
-                            price = "1500",
-                            isFavorite = true,
-                            productName = "iPhone 13 Pro Max 256Gb"
-                        )
+                if (uiState.products.isNullOrEmpty().not()) {
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(2),
+                        horizontalArrangement = Arrangement.spacedBy(21.dp),
+                        verticalArrangement = Arrangement.spacedBy(14.dp),
+                    ) {
+                        items(uiState.products!!) { product ->
+                            ProductCard(
+                                imageUrl = product?.image,
+                                price = product?.price,
+                                isFavorite = true,
+                                productName = product?.name
+                            )
+                        }
                     }
                 }
             }
         }
-
     }
 }
