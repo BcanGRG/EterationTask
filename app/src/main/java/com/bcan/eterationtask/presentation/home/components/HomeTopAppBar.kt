6262278937688709ce34115c075de692ddb6566a
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-09 09:13:47 +0300

    created TopAppbar

---
+++ app/src/main/java/com/bcan/eterationtask/presentation/home/components/HomeTopAppBar.kt
@@ -0,0 +1,27 @@
+package com.bcan.eterationtask.presentation.home.components
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.bcan.eterationtask.ui.theme.BlueRibbon
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopAppBar() {
+    TopAppBar(
+        title = {
+            Text(
+                "E-Market",
+                color = Color.White,
+                fontSize = 24.sp,
+                fontWeight = FontWeight(800)
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors().copy(containerColor = BlueRibbon),
+    )
+}
