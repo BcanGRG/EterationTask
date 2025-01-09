aac44cce6512c3bfdabeacab2bbbbcf85585a911
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-09 10:43:24 +0300

    added silver,alto and selectiveyellow colors

--- app/src/main/java/com/bcan/eterationtask/ui/theme/Color.kt
+++ app/src/main/java/com/bcan/eterationtask/ui/theme/Color.kt
@@ -11,3 +11,6 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val BlueRibbon = Color(0xFF2A59FE)
+val Silver = Color(0xFFC4C4C4)
+val Alto = Color(0xFFD9D9D9)
+val SelectiveYellow = Color(0xFFFFB800)
