efa5a3c49c30bdf86200b4881f3b27c4f675f8d1
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-10 00:01:57 +0300

    deleted dao functions

--- app/src/main/java/com/bcan/eterationtask/data/repository/ProductsRepository.kt
+++ app/src/main/java/com/bcan/eterationtask/data/repository/ProductsRepository.kt
@@ -1,16 +1,10 @@
 package com.bcan.eterationtask.data.repository
 
-import com.bcan.eterationtask.data.model.ProductResponseModel
-import com.bcan.eterationtask.data.model.ProductResponseModelDao
+import com.bcan.eterationtask.data.domain.model.ProductResponseModel
 import com.bcan.eterationtask.data.util.NetworkResult
 import kotlinx.coroutines.flow.Flow
 
 interface ProductsRepository {
 
     suspend fun getProducts(): Flow<NetworkResult<List<ProductResponseModel>>>
-    suspend fun addProduct(product: ProductResponseModelDao)
-    suspend fun deleteProduct(product: ProductResponseModelDao)
-    suspend fun increaseQuantity(productId: String, amount: Int)
-    suspend fun decreaseQuantity(productId: String, amount: Int)
-    suspend fun getBasketProducts(): Flow<List<ProductResponseModelDao>>
 }
--- app/src/main/java/com/bcan/eterationtask/data/repository/ProductsRepositoryImpl.kt
+++ app/src/main/java/com/bcan/eterationtask/data/repository/ProductsRepositoryImpl.kt
@@ -1,8 +1,8 @@
 package com.bcan.eterationtask.data.repository
 
 import com.bcan.eterationtask.data.local.ProductDao
-import com.bcan.eterationtask.data.model.ProductResponseModel
-import com.bcan.eterationtask.data.model.ProductResponseModelDao
+import com.bcan.eterationtask.data.domain.model.ProductResponseModel
+import com.bcan.eterationtask.data.domain.model.ProductResponseModelDao
 import com.bcan.eterationtask.data.service.ProductsService
 import com.bcan.eterationtask.data.util.NetworkResult
 import com.bcan.eterationtask.data.util.sendRequestWithResponse
@@ -14,41 +14,8 @@ import javax.inject.Inject
 
 class ProductsRepositoryImpl @Inject constructor(
     private val productsService: ProductsService,
-    private val productDao: ProductDao,
 ) : ProductsRepository {
     override suspend fun getProducts(): Flow<NetworkResult<List<ProductResponseModel>>> {
         return sendRequestWithResponse { productsService.getProducts() }
     }
-
-    override suspend fun addProduct(product: ProductResponseModelDao) {
-        val existingProduct = productDao.getProductById(product.id)
-        if (existingProduct != null) {
-            productDao.increaseProductQuantity(product.id, product.quantity)
-        } else productDao.insertProduct(product)
-    }
-
-    override suspend fun deleteProduct(product: ProductResponseModelDao) {
-        productDao.deleteProduct(product)
-    }
-
-    override suspend fun increaseQuantity(productId: String, amount: Int) {
-        productDao.increaseProductQuantity(productId, amount)
-    }
-
-    override suspend fun decreaseQuantity(productId: String, amount: Int) {
-        val currentProduct = productDao.getProductById(productId) ?: return
-        if (currentProduct.quantity <= amount) {
-            productDao.deleteProduct(currentProduct)
-        } else {
-            productDao.decreaseProductQuantity(productId, amount)
-        }
-    }
-
-    override suspend fun getBasketProducts(): Flow<List<ProductResponseModelDao>> {
-        return flow {
-            emit(productDao.getAllProducts())
-        }.flowOn(Dispatchers.IO)
-    }
-
-
 }
