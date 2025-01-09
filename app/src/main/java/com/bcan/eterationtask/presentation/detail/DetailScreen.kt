7f3b54c8b84cf79dc26535fef9d78500b11d8525
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-09 16:36:40 +0300

    added surface

--- app/src/main/java/com/bcan/eterationtask/presentation/detail/DetailScreen.kt
+++ app/src/main/java/com/bcan/eterationtask/presentation/detail/DetailScreen.kt
@@ -1,12 +1,16 @@
 package com.bcan.eterationtask.presentation.detail
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.bcan.eterationtask.data.model.ProductResponseModel
-import com.bcan.eterationtask.presentation.home.components.DetailTopAppBar
+import com.bcan.eterationtask.presentation.detail.components.DetailTopAppBar
 
 @Composable
 fun DetailScreen(
@@ -21,9 +25,16 @@ fun DetailScreen(
                 onBackClick = onBackClick
             )
         }
-    ) {
-        Column(modifier = Modifier.padding(it)) {
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            color = Color.White
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
 
+            }
         }
     }
 
