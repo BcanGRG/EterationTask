8deaecfb5d32c63b894b104bc68224958c7193b2
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-10 00:03:31 +0300

    deleted imports

--- app/src/main/java/com/bcan/eterationtask/data/repository/ProductsRepositoryImpl.kt
+++ app/src/main/java/com/bcan/eterationtask/data/repository/ProductsRepositoryImpl.kt
@@ -1,15 +1,10 @@
 package com.bcan.eterationtask.data.repository
 
-import com.bcan.eterationtask.data.local.ProductDao
 import com.bcan.eterationtask.data.domain.model.ProductResponseModel
-import com.bcan.eterationtask.data.domain.model.ProductResponseModelDao
 import com.bcan.eterationtask.data.service.ProductsService
 import com.bcan.eterationtask.data.util.NetworkResult
 import com.bcan.eterationtask.data.util.sendRequestWithResponse
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class ProductsRepositoryImpl @Inject constructor(
