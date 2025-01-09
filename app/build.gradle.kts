6f8fb294f9bbb9bb11867bcce9dbcc4fde4a0c0a
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-09 09:26:01 +0300

    added serialization library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -80,4 +81,8 @@ dependencies {
 
     // Lottie
     implementation(libs.lottie.compose)
+
+    // Serialization
+    implementation(libs.kotlinx.serialization.core)
+    implementation(libs.kotlinx.serialization.json)
 }
