ecd909254bb86bf004403c09b235846ab20a4e28
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-09 23:40:06 +0300

    added snackbar one time event properties

--- app/src/main/java/com/bcan/eterationtask/presentation/navigation/EterationTaskNavigation.kt
+++ app/src/main/java/com/bcan/eterationtask/presentation/navigation/EterationTaskNavigation.kt
@@ -7,9 +7,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -19,6 +25,9 @@ import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.bcan.eterationtask.presentation.ui.snackbar.ObserveAsEvents
+import com.bcan.eterationtask.presentation.ui.snackbar.SnackbarController
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -28,6 +37,23 @@ fun EterationTaskNavigation() {
     val entry by navController.currentBackStackEntryAsState()
     val currentDestination = entry?.destination
 
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    ObserveAsEvents(flow = SnackbarController.events, snackbarHostState) { event ->
+        scope.launch {
+            snackbarHostState.currentSnackbarData?.dismiss()
+            val result = snackbarHostState.showSnackbar(
+                message = event.message,
+                actionLabel = event.action?.name,
+                duration = SnackbarDuration.Short,
+            )
+
+            if (result == SnackbarResult.ActionPerformed) {
+                event.action?.action?.invoke()
+            }
+        }
+
+    }
 
     Scaffold(
         topBar = { TopAppBar(modifier = Modifier.height(0.dp), title = {}) },
@@ -51,7 +77,11 @@ fun EterationTaskNavigation() {
                     )
                 }
             }
-        }) {
+        },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }
+    ) {
         NavHost(
             modifier = Modifier.padding(it),
             navController = navController,
