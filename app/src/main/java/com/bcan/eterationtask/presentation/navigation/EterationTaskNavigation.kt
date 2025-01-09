d21e1a2aaa34edfaa1632d468982f35418210619
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-09 16:27:43 +0300

    added DetailRoute and passed data

--- app/src/main/java/com/bcan/eterationtask/presentation/navigation/EterationTaskNavigation.kt
+++ app/src/main/java/com/bcan/eterationtask/presentation/navigation/EterationTaskNavigation.kt
@@ -4,7 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.bcan.eterationtask.data.model.ProductResponseModel
+import com.bcan.eterationtask.presentation.detail.DetailScreen
 import com.bcan.eterationtask.presentation.home.HomeScreen
+import com.bcan.eterationtask.presentation.utils.CustomNavType
+import kotlin.reflect.typeOf
 
 @Composable
 fun EterationTaskNavigation() {
@@ -12,10 +17,22 @@ fun EterationTaskNavigation() {
 
     NavHost(
         navController = navController,
-        startDestination = Home,
+        startDestination = HomeRoute,
     ) {
-        composable<Home> {
-            HomeScreen()
+        composable<HomeRoute> {
+            HomeScreen(
+                navigateToDetail = { navController.navigate(DetailRoute(it)) }
+            )
+        }
+
+        composable<DetailRoute>(
+            typeMap = mapOf(typeOf<ProductResponseModel?>() to CustomNavType.ProductType),
+        ) {
+            val arguments = it.toRoute<DetailRoute>()
+            DetailScreen(
+                product = arguments.product,
+                onBackClick = { navController.popBackStack() }
+            )
         }
     }
 }
