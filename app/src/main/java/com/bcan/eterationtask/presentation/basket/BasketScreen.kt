1401f29d37a7637a4b3d239253f7fa326b6faea6
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-09 20:39:32 +0300

    created other screens

---
+++ app/src/main/java/com/bcan/eterationtask/presentation/basket/BasketScreen.kt
@@ -0,0 +1,9 @@
+package com.bcan.eterationtask.presentation.basket
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BasketScreen() {
+    Text("Basket Screen")
+}
---
+++ app/src/main/java/com/bcan/eterationtask/presentation/favorite/FavoriteScreen.kt
@@ -0,0 +1,12 @@
+package com.bcan.eterationtask.presentation.favorite
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FavoriteScreen() {
+    Text("Favorite Screen", modifier = Modifier.padding(top = 60.dp))
+}
