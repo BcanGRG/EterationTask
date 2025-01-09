02f2a28b98664b4ab6b9f28abad9b5b5a048d128
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-09 09:14:23 +0300

    applied HomeScreen and statusBarColor

--- app/src/main/java/com/bcan/eterationtask/MainActivity.kt
+++ app/src/main/java/com/bcan/eterationtask/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import com.bcan.eterationtask.presentation.home.HomeScreen
 import com.bcan.eterationtask.ui.theme.EterationTaskTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EterationTaskTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                window.statusBarColor = Color.White.toArgb()
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EterationTaskTheme {
-        Greeting("Android")
     }
 }
