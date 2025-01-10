fb9538d2ad566562879c1bc4f706befc63cda505
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-10 12:03:15 +0300

    created animations

---
+++ app/src/main/java/com/bcan/eterationtask/presentation/ui/animations/ErrorAnimation.kt
@@ -0,0 +1,32 @@
+package com.bcan.eterationtask.presentation.ui.animations
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.bcan.eterationtask.R
+
+@Composable
+fun ErrorAnimation(modifier: Modifier = Modifier) {
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.error_animation)
+    )
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        LottieAnimation(
+            composition = composition,
+            iterations = LottieConstants.IterateForever,
+            isPlaying = true,
+            modifier = modifier
+        )
+    }
+}
