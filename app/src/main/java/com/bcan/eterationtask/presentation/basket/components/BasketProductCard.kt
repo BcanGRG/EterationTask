4b431395acb25531b04c27b175854674fe4c8994
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-10 12:01:55 +0300

    edited sizes

--- app/src/main/java/com/bcan/eterationtask/presentation/basket/components/BasketProductCard.kt
+++ app/src/main/java/com/bcan/eterationtask/presentation/basket/components/BasketProductCard.kt
@@ -3,10 +3,12 @@ package com.bcan.eterationtask.presentation.basket.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bcan.eterationtask.ui.theme.BlueRibbon
 
@@ -19,7 +21,7 @@ fun BasketProductCard(
     onDecrease: () -> Unit
 ) {
     Row {
-        Column {
+        Column(modifier = Modifier.width(180.dp)) {
             Text(
                 text = name,
                 fontWeight = FontWeight(400),
@@ -34,7 +36,6 @@ fun BasketProductCard(
             )
         }
         Spacer(modifier = Modifier.weight(1f))
-
         IncreaseDecreaseBox(
             text = "-",
             onClick = onDecrease
@@ -46,5 +47,6 @@ fun BasketProductCard(
             text = "+",
             onClick = onIncrease
         )
+
     }
 }
